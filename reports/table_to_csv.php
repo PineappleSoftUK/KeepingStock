3226353aa32d823d21a19607fa9e105013a8739d
David Ward <admin@pineapplesoft.co.uk>
2020-08-22 10:29:59 +0100

    Preperations to allow table name to be passed

--- reports/table_to_csv.php
+++ reports/table_to_csv.php
@@ -1,4 +1,8 @@
 <?php
+/**
+ * Accepts a table name via HTTP Post 
+ * Outputs a csv file download with SQLite table contents
+ */
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
@@ -6,28 +10,46 @@ error_reporting(E_ALL);
 include("../secure.php");
 include("../open_db.php");
 
-// output headers so that the file is downloaded rather than displayed
-header('Content-Type: text/csv; charset=utf-8');
-header('Content-Disposition: attachment; filename=data.csv');
+// Set table name
+$validTables = array();
 
-// create a file pointer connected to the output stream
+$tablesquery = $db->query("SELECT name FROM sqlite_master WHERE type='table';");
+
+while ($table = $tablesquery->fetchArray(SQLITE3_ASSOC)) {
+    array_push($validTables, $table['name']);
+}
+
+//$hash = $_POST['table'];
+$hash = "postage";
+
+if (array_search($hash, $validTables)) {
+  $table = $hash;
+} else {
+  $table = "";
+}
+
+// This ensures the file is downloaded rather than displayed
+header('Content-Type: text/csv; charset=utf-8');
+header('Content-Disposition: attachment; filename=export.csv');
+
+// Create the file
 $output = fopen('php://output', 'w');
 
-//Get column names
+// Get column names
 $colNames = array();
-$res = $db->query('PRAGMA table_info(postage);'); 
+$res = $db->query('PRAGMA table_info(' . $table . ');'); 
 
 while ($row = $res->fetchArray()) {
     array_push($colNames, $row['name']);
 }
 
-// output the column headings
+// Output the column headings
 fputcsv($output, $colNames);
 
-// fetch the data
-$res = $db->query('SELECT * FROM postage');
+// Fetch table rows
+$res = $db->query("SELECT * FROM " . $table);
 
-// loop over the rows, outputting them
+// Write rows to file
 while ($row = $res->fetchArray(SQLITE3_ASSOC)) {
   fputcsv($output, $row);
 }
