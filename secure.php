342ec1558f54ffa63e8bc5ba4d967ee290524cb3
David Ward <admin@pineapplesoft.co.uk>
2020-08-22 11:00:52 +0100

    Change to link to allow access from different directories

--- secure.php
+++ secure.php
@@ -7,8 +7,8 @@ session_start();
 
 if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] != true) {
   $_SESSION['refer'] = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
-  header( "refresh:2;url=pineappleaccesscontrol/login.php" );
-  echo 'You must log in to view this page, You will now be redirected automatically, or, click <a href="pineappleaccesscontrol/login.php">here</a>.';
+  header( "refresh:2;url=" . __DIR__ . "/pineappleaccesscontrol/login.php" );
+  echo 'You must log in to view this page, You will now be redirected automatically, or, click <a href="' . __DIR__ . '/pineappleaccesscontrol/login.php">here</a>.';
   exit();
 } else {
   $pac = "yes"; //Displays log out button
