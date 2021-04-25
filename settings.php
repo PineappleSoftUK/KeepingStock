77f7811c0e98656ac5e41c7c54c1ad8c9ce82204
David Ward <admin@pineapplesoft.co.uk>
2021-04-25 13:42:41 +0100

    Updated version text

--- settings.php
+++ settings.php
@@ -3,13 +3,13 @@ include("secure.php");
 include("open_db.php");
 
 if (isset($_POST['submit'])) {
-  
+
   $packagingfee = $_POST['packagingfee'];
   $margin = $_POST['margin'];
   $ebaypercentage = $_POST['ebaypercentage'];
   $paypalpercentage = $_POST['paypalpercentage'];
   $paypalflatfee = $_POST['paypalflatfee'];
-  
+
   $stmt = $db->prepare("UPDATE settings SET packagingfee = :packagingfee, margin = :margin, ebaypercentage = :ebaypercentage, paypalpercentage = :paypalpercentage, paypalflatfee = :paypalflatfee WHERE id = 1");
   $stmt->bindValue(':packagingfee', $packagingfee);
   $stmt->bindValue(':margin', $margin);
@@ -17,7 +17,7 @@ if (isset($_POST['submit'])) {
   $stmt->bindValue(':paypalpercentage', $paypalpercentage);
   $stmt->bindValue(':paypalflatfee', $paypalflatfee);
   $result = $stmt->execute();
-  
+
 ?>
 
 <!DOCTYPE html>
@@ -26,7 +26,7 @@ if (isset($_POST['submit'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Keeping Stock</title>
-    
+
     <link rel="stylesheet" type="text/css" href="style.css">
     <!-- Load an icon library to show a hamburger menu (bars) on small screens -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
@@ -38,7 +38,7 @@ if (isset($_POST['submit'])) {
       <a href="purchase.php">Purchase</a>
       <a href="stock.php">Stock</a>
       <a href="settings.php" class="active">Settings</a>
-      <?php 
+      <?php
       if ($pac = "yes") {
         echo "<a href='pineappleaccesscontrol/logout.php' style='float:right;'>Log out</a>";
       }
@@ -49,10 +49,10 @@ if (isset($_POST['submit'])) {
     </div>
     <h1>Keeping Stock</h1>
     <h2>Settings</h2>
-    
+
     <p>The settings have now been updated.</p>
-    <p>Would you like to <a href="settings.php">edit again?</a> or return to <a href="index.php">home</a> 
-      
+    <p>Would you like to <a href="settings.php">edit again?</a> or return to <a href="index.php">home</a>
+
     <script>
     /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
     function myFunction() {
@@ -64,12 +64,12 @@ if (isset($_POST['submit'])) {
       }
     }
     </script>
-    
+
   </body>
 </html>
 
 <?php
-  
+
   exit();
 }
 ?>
@@ -80,7 +80,7 @@ if (isset($_POST['submit'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Keeping Stock</title>
-    
+
     <link rel="stylesheet" type="text/css" href="style.css">
     <!-- Load an icon library to show a hamburger menu (bars) on small screens -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
@@ -92,7 +92,7 @@ if (isset($_POST['submit'])) {
       <a href="purchase.php">Purchase</a>
       <a href="stock.php">Stock</a>
       <a href="settings.php" class="active">Settings</a>
-      <?php 
+      <?php
       if ($pac = "yes") {
         echo "<a href='pineappleaccesscontrol/logout.php' style='float:right;'>Log out</a>";
       }
@@ -102,45 +102,45 @@ if (isset($_POST['submit'])) {
       </a>
     </div>
     <h1>Keeping Stock</h1>
-    <p>Version 1.2.0 - Released 4 June 2020</p>
+    <p>Version 1.2.1 - Released 25 April 2021</p>
     <p>Includes PineappleAccessControl Version 1.0.2</p>
     <hr>
     <h2>Settings</h2>
-        
+
     <a href="postage_editor.php">Click here to adjust postage rates</a>
-    
+
     <form action="settings.php" method="post">
-    <br>  
-      
+    <br>
+
       <?php
       //SQLite query to pull all settings from the system and display as form
       $res = $db->query('SELECT * FROM settings');
-      
+
       while ($row = $res->fetchArray()) {
         //PARANTHESES REMAIN OPEN FOR USE IN HTML BELOW
       ?>
-      
+
       Packaging Fee:
       <input type="number" step="0.01" name="packagingfee" value="<?php echo $row['packagingfee'];?>"><br>
-      
+
       Margin:
       <input type="number" step="0.01" name="margin" value="<?php echo $row['margin'];?>"><br>
-      
+
       eBay Fees (Percentage as a decimal e.g 0.10 = 10%):
       <input type="number" step="0.01" name="ebaypercentage" value="<?php echo $row['ebaypercentage'];?>"><br>
-      
+
       PayPal Fees (Percentage as a decimal e.g 0.10 = 10%):
       <input type="number" step="0.001" name="paypalpercentage" value="<?php echo $row['paypalpercentage'];?>"><br>
-      
+
       PayPal Fees (Flat fee):
       <input type="number" step="0.01" name="paypalflatfee" value="<?php echo $row['paypalflatfee'];?>"><br>
       <?php
-      } //End of query!  
+      } //End of query!
       ?>
-      
+
       <input type="submit" name="submit" value="Submit">
     </form>
-    
+
     <script>
       /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
       function myFunction() {
@@ -152,6 +152,6 @@ if (isset($_POST['submit'])) {
         }
       }
     </script>
-    
+
   </body>
 </html>
