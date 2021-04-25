c32e48c08422162424e77d6f0cdf245fc1842a2f
David Ward <admin@pineapplesoft.co.uk>
2021-04-25 13:31:27 +0100

    Update to Royal Mail prices issued 1st Jan 2021

--- update_postage.php
+++ update_postage.php
@@ -2,51 +2,51 @@
 include("secure.php");
 include("open_db.php");
 $types = array(
-  
-  "1st class stamp" => 0.76,
-  "2nd class stamp" => 0.65,
-  "2nd class large letter 100g" => 0.88,
-  "2nd class large letter 250g" => 1.40,
-  "2nd class large letter 500g" => 1.83,
-  "2nd class large letter 750g" => 2.48,
-  "2nd class small parcel 1kg" => 3.10,
-  "2nd class small parcel 2kg" => 3.10,
-  "1st class large letter 100g" => 1.15,
-  "1st class large letter 250g" => 1.64,
-  "1st class large letter 500g" => 2.14,
-  "1st class large letter 750g" => 2.95,
-  "1st class small parcel 1kg" => 3.70,
+
+  "1st class stamp" => 0.85,
+  "2nd class stamp" => 0.66,
+  "2nd class large letter 100g" => 0.96,
+  "2nd class large letter 250g" => 1.53,
+  "2nd class large letter 500g" => 1.99,
+  "2nd class large letter 750g" => 2.70,
+  "2nd class small parcel 1kg" => 3.20,
+  "2nd class small parcel 2kg" => 3.20,
+  "1st class large letter 100g" => 1.29,
+  "1st class large letter 250g" => 1.83,
+  "1st class large letter 500g" => 2.39,
+  "1st class large letter 750g" => 3.30,
+  "1st class small parcel 1kg" => 3.85,
   "1st class small parcel 2kg" => 5.57,
-  "2nd class medium parcel 1kg" => 5.50,
-  "2nd class medium parcel 2kg" => 5.20,
+  "2nd class medium parcel 1kg" => 5.30,
+  "2nd class medium parcel 2kg" => 5.30,
   "2nd class medium parcel 5kg" => 8.99,
   "2nd class medium parcel 10kg" => 20.25,
   "2nd class medium parcel 20kg" => 28.55,
-  "1st class medium parcel 1kg" => 5.90,
+  "1st class medium parcel 1kg" => 6.00,
   "1st class medium parcel 2kg" => 9.02,
   "1st class medium parcel 5kg" => 15.85,
   "1st class medium parcel 10kg" => 21.90,
   "1st class medium parcel 20kg" => 33.40,
-  "Signed for - 1st class stamp" => 2.06,
-  "Signed for - 2nd class stamp" => 1.95,
-  "Signed for - 2nd class large letter 100g" => 2.18,
-  "Signed for - 2nd class large letter 250g" => 2.70,
-  "Signed for - 2nd class large letter 500g" => 3.13,
-  "Signed for - 2nd class large letter 750g" => 3.78,
-  "Signed for - 2nd class small parcel 1kg" => 4.10,
-  "Signed for - 2nd class small parcel 2kg" => 4.10,
-  "Signed for - 1st class large letter 100g" => 2.45,
-  "Signed for - 1st class large letter 250g" => 2.94,
-  "Signed for - 1st class large letter 500g" => 3.44,
-  "Signed for - 1st class large letter 750g" => 4.25,
-  "Signed for - 1st class small parcel 1kg" => 4.70,
+  "Signed for - 1st class stamp" => 2.25,
+  "Signed for - 2nd class stamp" => 2.06,
+  "Signed for - 2nd class large letter 100g" => 2.36,
+  "Signed for - 2nd class large letter 250g" => 2.93,
+  "Signed for - 2nd class large letter 500g" => 3.39,
+  "Signed for - 2nd class large letter 750g" => 4.10,
+  "Signed for - 2nd class small parcel 1kg" => 4.20,
+  "Signed for - 2nd class small parcel 2kg" => 4.20,
+  "Signed for - 1st class large letter 100g" => 2.69,
+  "Signed for - 1st class large letter 250g" => 3.23,
+  "Signed for - 1st class large letter 500g" => 3.79,
+  "Signed for - 1st class large letter 750g" => 4.70,
+  "Signed for - 1st class small parcel 1kg" => 4.85,
   "Signed for - 1st class small parcel 2kg" => 6.57,
-  "Signed for - 2nd class medium parcel 1kg" => 6.20,
-  "Signed for - 2nd class medium parcel 2kg" => 6.20,
+  "Signed for - 2nd class medium parcel 1kg" => 6.30,
+  "Signed for - 2nd class medium parcel 2kg" => 6.30,
   "Signed for - 2nd class medium parcel 5kg" => 9.99,
   "Signed for - 2nd class medium parcel 10kg" => 21.25,
   "Signed for - 2nd class medium parcel 20kg" => 29.55,
-  "Signed for - 1st class medium parcel 1kg" => 6.90,
+  "Signed for - 1st class medium parcel 1kg" => 7.00,
   "Signed for - 1st class medium parcel 2kg" => 10.02,
   "Signed for - 1st class medium parcel 5kg" => 16.85,
   "Signed for - 1st class medium parcel 10kg" => 22.90,
@@ -59,7 +59,7 @@ $types = array(
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Keeping Stock</title>
-    
+
     <link rel="stylesheet" type="text/css" href="style.css">
     <!-- Load an icon library to show a hamburger menu (bars) on small screens -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
@@ -71,7 +71,7 @@ $types = array(
       <a href="purchase.php">Purchase</a>
       <a href="stock.php">Stock</a>
       <a href="settings.php" class="active">Settings</a>
-      <?php 
+      <?php
       if ($pac = "yes") {
         echo "<a href='pineappleaccesscontrol/logout.php' style='float:right;'>Log out</a>";
       }
@@ -91,9 +91,9 @@ $types = array(
     <form action="update_postage.php" method="post">
       <input type="submit" name="submit" value="Update">
     </form>
-    <?php  
+    <?php
     } else {
-    
+
       //If confirmed...
       $stmt = $db->prepare('UPDATE postage SET price= :price WHERE description= :description');
 
@@ -103,9 +103,9 @@ $types = array(
         $result = $stmt->execute();
       }
     ?>
-    
+
     <p>The rates have been successfully updated.</p>
-    
+
     <?php
     }
     ?>
@@ -120,6 +120,6 @@ $types = array(
         }
       }
     </script>
-    
+
   </body>
 </html>
